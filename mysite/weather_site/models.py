cb59fc9944a5f9a89b91be02e511a1ba214b6c56
Artur_Lazarevich <compact20002008@gmail.com>
2025-07-02 14:07:35 +0300

    create django

---
+++ mysite/weather_site/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=2)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    last_update = models.DateTimeField(auto_now=True)
+
+class WeatherData(models.Model):
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    temperature = models.FloatField()
+    feels_like = models.FloatField()
+    description = models.CharField(max_length=255)
+    icon = models.CharField(max_length=10)
+    timestamp = models.DateTimeField(auto_now_add=True)
---
+++ mysite/weather_site/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+# from .views import WeatherView
+
+
+app_name = "weather_site"
+
+urlpatterns = [
+    # path("", WeatherView.as_view(), name="weather"),
+]
